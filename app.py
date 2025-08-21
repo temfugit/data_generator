f656ebeb4e3b6796e7c6353440e4df90e77888ed
temfugit <127996760+temfugit@users.noreply.github.com>
2025-08-21 21:31:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import random
 import string
 
 app = Flask(__name__)
+CORS(app)  # <--- allow all origins for now
 
 def random_name(length=5):
     return ''.join(random.choices(string.ascii_letters, k=length))
